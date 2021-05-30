7d23decd8d169e07de72b16f09ad47d6d9f6b829
soethan98 <soethan762@gmail.com>
2021-05-30 19:18:07 +0630

    user login system

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 
 
 
-# Create your views here.
+@login_required
 def home(request):
     context= { 
         'posts': Post.objects.all()
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
 
 def register(request):
     if request.method == 'POST':
@@ -9,9 +11,17 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(
+                request, 'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request,'users/register.html',{'form':form})
+    return render(request, 'users/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
+
 
